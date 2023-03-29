ddb38c562b463388f79476bc25057087eca4256b
Haochen M. Kotoi-Xie <hx@kxc.inc>
2023-03-30 04:13:06 +0900

    Revert "fix regenerate"
    This reverts commit 4a8cab7b0c45b79350cc6e7e8a658e899ac22ca7.

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -1,6 +1,5 @@
 import { Message, OpenAIModel, OpenAIModelID } from '@/types';
 import { IconPlayerStop, IconRepeat, IconSend } from '@tabler/icons-react';
-import { useTranslation } from 'next-i18next';
 import {
   FC,
   KeyboardEvent,
@@ -8,6 +7,7 @@ import {
   useEffect,
   useState,
 } from 'react';
+import { useTranslation } from 'next-i18next';
 
 interface Props {
   messageIsStreaming: boolean;
