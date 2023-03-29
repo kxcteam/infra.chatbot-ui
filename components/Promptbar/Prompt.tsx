598d09491b54da0226a0e18aaca26c1f89d9e00c
Haochen M. Kotoi-Xie <hx@kxc.inc>
2023-03-30 04:13:06 +0900

    Revert "update prompts to be tabbable (#241)"
    This reverts commit 3b0006d1afd0cc2a9a83848ae3c5c9d8f807ce53.

--- components/Promptbar/Prompt.tsx
+++ components/Promptbar/Prompt.tsx
@@ -39,9 +39,9 @@ export const PromptComponent: FC<Props> = ({
   }, [isRenaming, isDeleting]);
 
   return (
-    <div className="relative flex items-center">
+    <>
       <button
-        className="flex w-full cursor-pointer items-center gap-3 rounded-lg p-3 text-sm transition-colors duration-200 hover:bg-[#343541]/90"
+        className="text-sidebar flex w-full cursor-pointer items-center gap-3 rounded-lg p-3 text-[14px] transition-colors duration-200 hover:bg-[#343541]/90"
         draggable="true"
         onClick={(e) => {
           e.stopPropagation();
@@ -54,17 +54,27 @@ export const PromptComponent: FC<Props> = ({
           setRenameValue('');
         }}
       >
-        <IconBulbFilled size={18} />
+        <IconBulbFilled size={16} />
 
-        <div className="relative max-h-5 flex-1 overflow-hidden text-ellipsis whitespace-nowrap break-all text-left text-[12.5px] leading-3">
-          {prompt.name}
-        </div>
-      </button>
+        {isRenaming ? (
+          <input
+            className="flex-1 overflow-hidden overflow-ellipsis border-b border-neutral-400 bg-transparent pr-1 text-left text-white outline-none focus:border-neutral-100"
+            type="text"
+            value={renameValue}
+            onChange={(e) => setRenameValue(e.target.value)}
+            autoFocus
+          />
+        ) : (
+          <div className="flex-1 overflow-hidden overflow-ellipsis whitespace-nowrap pr-1 text-left">
+            {prompt.name}
+          </div>
+        )}
 
         {(isDeleting || isRenaming) && (
-          <div className="absolute right-1 z-10 flex text-gray-300">
-            <button
-              className="min-w-[20px] p-1 text-neutral-400 hover:text-neutral-100"
+          <div className="-ml-2 flex gap-1">
+            <IconCheck
+              className="min-w-[20px] text-neutral-400 hover:text-neutral-100"
+              size={16}
               onClick={(e) => {
                 e.stopPropagation();
 
@@ -74,35 +84,32 @@ export const PromptComponent: FC<Props> = ({
 
                 setIsDeleting(false);
               }}
-            >
-              <IconCheck size={18} />
-            </button>
+            />
 
-            <button
-              className="min-w-[20px] p-1 text-neutral-400 hover:text-neutral-100"
+            <IconX
+              className="min-w-[20px] text-neutral-400 hover:text-neutral-100"
+              size={16}
               onClick={(e) => {
                 e.stopPropagation();
                 setIsDeleting(false);
               }}
-            >
-              <IconX size={18} />
-            </button>
+            />
           </div>
         )}
 
         {!isDeleting && !isRenaming && (
-          <div className="absolute right-1 z-10 flex text-gray-300">
-            <button
-              className="min-w-[20px] p-1 text-neutral-400 hover:text-neutral-100"
+          <div className="-ml-2 flex gap-1">
+            <IconTrash
+              className=" min-w-[20px] text-neutral-400 hover:text-neutral-100"
+              size={18}
               onClick={(e) => {
                 e.stopPropagation();
                 setIsDeleting(true);
               }}
-            >
-              <IconTrash size={18} />
-            </button>
+            />
           </div>
         )}
+      </button>
 
       {showModal && (
         <PromptModal
@@ -111,6 +118,6 @@ export const PromptComponent: FC<Props> = ({
           onUpdatePrompt={onUpdatePrompt}
         />
       )}
-    </div>
+    </>
   );
 };
